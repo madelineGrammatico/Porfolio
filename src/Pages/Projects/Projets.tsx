d2f0ec3c6816edeff3c3f5804392c53690257239
Madeline <grammaticomadeline@hotmail.fr>
2023-10-20 20:24:15 +0200

    fix(app) : refactoring projects

--- src/Pages/Projects/Projets.tsx
+++ src/Pages/Projects/Projets.tsx
@@ -9,13 +9,92 @@ import {
 import { addFilter, initilizeFilter, removeFilter } from "../../app/filtersSlice/filtersSlice";
 import { useEffect } from "react";
 import { Project } from "../../components/Project/Project";
+import styled from "styled-components"
+import { ButtonLink } from "../../components/Button/Button";
+import colors from "../../color";
 
 const traduction = {
     pageTitle: {
         EN: "Projects",
-        FR: "Projets"
+        FR: "Projets",
+    },
+    filterChoice: {
+        EN: "Choose a technologie as a filter :",
+        FR: "Choisissez une technologie comme filtre :",
+    },
+    resetButton: {
+        EN:"Reset Filters",
+        FR: "Réinitialiser Filtres",
     }
 }
+const ProjectsContainer = styled.section`
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 2rem;
+        padding: 6rem 10rem ;
+        background-color: ${colors.third};
+    `
+    const FilterStyled = styled.button`
+        color: white;
+        background-color: ${colors.fourth};
+        padding: 1rem;
+        border-radius: 2rem;
+        cursor: pointer;
+        &:hover {
+            color:${colors.fourth};
+            background-color: white;
+            border-color: ${colors.fourth};
+        }
+    `
+    const TitleFilter = styled.h1`
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        color: white;
+        span {
+            color: ${colors.fourth};
+            background-color: white;
+            border: solid ${colors.fourth} 6px;
+            padding: 1.5rem;
+            border-radius: 4rem;
+            cursor: pointer;
+            
+            &:hover{
+                color: white;
+                background-color: ${colors.fourth};
+            }
+        }
+    `
+    const FilterContainer= styled.div`
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
+        gap: 1.3rem;
+        width: 100%;
+        padding:  0 4rem 2rem 4rem;
+        color: ${colors.first};
+    `
+    const ResetButton = styled(ButtonLink)`
+        color: ${colors.second};
+        background-color: ${colors.fourth};
+        border: solid 4px ${colors.second};
+        &:hover{
+            color:${colors.fourth};
+            background-color: white;
+            border: solid 4px white;
+        }
+    `
+    const HeaderProject = styled.header`
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        padding-bottom: 2rem;
+        background-color:${colors.fourth};
+    `
 export function Projects() {
     const projects = useSelector((state: RootState) => state.projectsSlice.projects)
     const filters = useSelector((state: RootState) => state.filtersSlice.filters)
@@ -51,6 +130,7 @@ export function Projects() {
 
     const removeFilterElement = (e: React.MouseEvent) => {
         const span = e.target as HTMLElement
+        console.log(span.innerHTML)
         dispatch(removeFilter(span.innerHTML))
         selectTechnos()
     }
@@ -58,27 +138,29 @@ export function Projects() {
     return (
         <main>
             <Nav/>
-            <h1>{ traduction.pageTitle[language] } 
-                {filters.map((filter, index) => 
-                    <span onClick={(e) => removeFilterElement(e)} key={index}>{filter}</span>
-                )}
-            </h1>
-            <div>
-                <input type="search"></input>
-                <div>
+            <HeaderProject>
+                <TitleFilter>{ traduction.pageTitle[language] } 
+                    { filters.map((filter, index) => 
+                        <span onClick={(e) => removeFilterElement(e)} key={index}>{filter}</span>
+                    )}
+                </TitleFilter>
+                <p>{traduction.filterChoice[language]}</p>
+                <FilterContainer>
                     {
                         tablTechno.map((techno, index)=> {
-                            return <p onClick={(e)=>filterByTechno(e)} key={index}>{techno}</p>
+                            return <FilterStyled onClick={(e)=>filterByTechno(e)} key={index}>{techno}</FilterStyled>
                         })
                     }
-                </div>
-            </div>
-            <button onClick={()=> resetProjects()}>reset</button>
-
-            { projects.map((project, index) => {
-                return <Project project={project} key={index} ></Project>
-            })}
-
+                     
+                </FilterContainer>
+                <ResetButton onClick={()=> resetProjects()}>{traduction.resetButton[language]}</ResetButton>
+            </HeaderProject>
+           
+            <ProjectsContainer>
+                { projects.map((project, index) => {
+                    return <Project project={project} key={index} ></Project>
+                })}
+            </ProjectsContainer>
         </main>
     )
 }
